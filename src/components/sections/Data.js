3d35178725d735f376d4063c880370f147b820a2
Syd McLean <syd@Syds-MacBook-Air.local>
2023-07-13 20:46:09 +0100

    Added data.js

---
+++ src/components/sections/Data.js
@@ -0,0 +1,19 @@
+export default Companies = {
+    type: "FeatureCollection",
+    features: [
+        {
+            type: "Feature",
+            "properties": {
+                name: "Syd",
+                logo: "syd.jpg"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    0, 0
+                ]
+            }
+        },
+        {}
+    ]
+}
