4a4fe2528f9fbe5a864b4e263b4481dda236d943
Tom McLean <64887735+mcleantom@users.noreply.github.com>
2023-07-14 22:08:07 +0100

    added info card and selected company

---
+++ src/components/sections/SelectedCompany.js
@@ -0,0 +1,41 @@
+import {
+  Card,
+  Image,
+  Stack,
+  CardBody,
+  Heading,
+  Text,
+  CardFooter,
+  Button,
+  Link,
+  Flex,
+} from "@chakra-ui/react";
+
+export default function SelectedCompany({ company }) {
+  return (
+    <Card overflow="hidden" width={"300px"}>
+      <Image
+        objectFit="cover"
+        height="125px"
+        src={company.logo}
+        alt="Caffe Latte"
+      />
+
+      <Stack>
+        <CardBody>
+          <Heading size="sm">{company.name}</Heading>
+          <Text size="sm" maxHeight="200px" overflowY="auto">
+            {company.description}
+          </Text>
+        </CardBody>
+        <CardFooter padding={4}>
+          <Link href={company.website}>
+            <Button variant="solid" colorScheme="blue">
+              Website
+            </Button>
+          </Link>
+        </CardFooter>
+      </Stack>
+    </Card>
+  );
+}
