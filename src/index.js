05c7ddb1f0a788e02ba83d389ba804663f5b1817
Tom McLean <tom.mclean@bartechnologies.uk>
2023-07-14 15:27:54 +0100

    added back theme provider

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <ChakraProvider> */}
+    <ChakraProvider>
       <App />
-    {/* </ChakraProvider> */}
+    </ChakraProvider>
   </React.StrictMode>
 );
 
