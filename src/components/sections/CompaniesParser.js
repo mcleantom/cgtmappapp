6b460acb3b997a06fa245eb35453bd1539507e51
Tom McLean <64887735+mcleantom@users.noreply.github.com>
2024-02-08 20:59:27 +0000

    use actual company position

--- src/components/sections/CompaniesParser.js
+++ src/components/sections/CompaniesParser.js
@@ -2,13 +2,11 @@ import axios from "axios";
 
 export default async function getCompanyData() {
   const response = await axios.get("https://cgtapi.mcleanmarine.com/company");
-  console.log(response.data);
-
   return response.data.map((company) => {
     return {
       name: company.name,
-      lat: 51.509865,
-      lon: -0.118092,
+      lat: company.position.coordinates[1],
+      lon: company.position.coordinates[0],
       logo: company.logo,
       description: company.description,
       website: company.website,
